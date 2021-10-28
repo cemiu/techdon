55b98611855b00167e00494d865f101b51fc94d6
Philipp <11079485+phbib@users.noreply.github.com>
2021-10-28 09:10:08 +0100

    Setup of Spring Boot & MongoDB access

---
+++ back-end/techdon/src/test/java/ac/brunel/techdon/TechDonApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.brunel.techdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechDonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
